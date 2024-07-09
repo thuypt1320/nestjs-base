f5341165330d33ae5bfb1cb3e591f84f09d57155
thuypt <thuypt@reactplus.com>
2024-07-09 18:11:12 +0700

    # Prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { PrismaController } from './prisma/prisma.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController, AuthController, UserController],
+  imports: [AuthModule, UserModule, PrismaModule],
+  controllers: [AppController, AuthController, UserController, PrismaController],
   providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
